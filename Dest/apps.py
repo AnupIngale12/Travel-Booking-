94ca38352e2d197bc49563f51446281d4e8dbb5a
coder <anuingale34@gmail.com>
2020-08-30 01:28:55 +0530

    Update Code

--- Dest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DestConfig(AppConfig):
-    name = 'Dest'
--- Dest/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Destination 
-
-def index(request):
-  dests = Destination.objects.all().order_by('price')
-  return render(request,'index.html',{'dests':dests})
